a25acc1592924a71c48eb39c7777d84eb3402876
Tom Whitwell <tom@whi.tw>
2022-12-23 12:42:10 +0000

    add health endpoint

--- arunapi/cache.py
+++ arunapi/cache.py
@@ -1,5 +1,5 @@
 import os
-from datetime import timedelta
+from datetime import datetime, timedelta
 from typing import Any, Tuple, Union
 
 from cashews import Cache as Cashews
@@ -16,7 +16,19 @@ class CacheSetFailed(Exception):
 class Cache:
     def __init__(self) -> None:
         self.cache = Cashews()
-        self.cache.setup(os.getenv("REDIS_URL"))
+        try:
+            self.cache.setup(os.environ["REDIS_URL"])
+        except KeyError:
+            import logging
+
+            uvicorn_logger = logging.getLogger("uvicorn")
+            uvicorn_logger.warning('Environment variable "REDIS_URL" not set.')
+            if os.getenv("MEMORY_CACHE"):
+                self.cache.setup("mem://?size=500")
+                uvicorn_logger.warning("Memory cache enabled by MEMORY_CACHE")
+            else:
+                self.cache.disable()
+                uvicorn_logger.warning("Cache disabled.")
 
     @staticmethod
     def build_key(key: str, namespace: str) -> str:
@@ -40,3 +52,14 @@ class Cache:
         return await self.cache.set(
             key=self.build_key(key, namespace), value=value, expire=ttl
         )
+
+    async def health(self) -> str:
+        key = value = datetime.now().isoformat()
+        cache_key = self.build_key(key, namespace="healthcheck")
+        await self.cache.set(key=cache_key, value=value)
+        retr_value = await self.cache.get(cache_key)
+        await self.cache.expire(cache_key, 1)
+        if retr_value == value:
+            return "OK"
+        else:
+            return "BAD"
--- arunapi/main.py
+++ arunapi/main.py
@@ -1,4 +1,6 @@
-from fastapi import Depends, FastAPI, Request, Response
+import logging
+
+from fastapi import FastAPI, Request, Response
 
 from . import __version__
 from .cache import Cache
@@ -31,3 +33,25 @@ app.include_router(refuse.router)
 async def init_cache():
     cache = Cache()
     app.state.cache = cache
+
+
+@app.on_event("startup")
+async def disable_logging_health_endpoint():
+    uvicorn_access_logger = logging.getLogger("uvicorn.access")
+
+    class HealthcheckFilter(logging.Filter):
+        def filter(self, record: logging.LogRecord) -> bool:
+            query_string: str = record.args[2]
+            status_code: int = record.args[4]
+            return not (query_string == "/health" and status_code == 200)
+
+    uvicorn_access_logger.addFilter(HealthcheckFilter())
+
+
+@app.get("/health", include_in_schema=False)
+async def health_endpoint(response: Response):
+    cache: Cache = app.state.cache
+    cache_health = await cache.health()
+    if cache_health != "OK":
+        response.status_code = 500
+    return {"cache": cache_health}
