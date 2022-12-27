f0568c65c27e93f80f4d894c3cc4a48f272fadb5
Tom Whitwell <tom@whi.tw>
2022-12-27 16:25:28 +0000

    poetry scripts for bump data

--- ci/bump_version.py
+++ ci/bump_version.py
@@ -14,7 +14,7 @@ def version_from_tag(tag: TagReference) -> str:
         return tag
 
 
-def main(repo: Repo, bump_type: str = "patch"):
+def _bump_data(repo: Repo = Repo(), bump_type: str = "patch") -> dict:
     latest_tag = repo.tags[-1]
 
     latest_version = version_from_tag(latest_tag)
@@ -28,19 +28,32 @@ def main(repo: Repo, bump_type: str = "patch"):
     next_version = bump_function()
     next_tag = f"v{str(next_version)}"
 
-    output_data = {
+    return {
         "latest_tag": str(latest_tag),
         "latest_version": latest_version,
         "next_tag": next_tag,
         "next_version": str(next_version),
     }
 
-    print(json.dumps(output_data))
+
+def bump_patch() -> str:
+    data = _bump_data(Repo(), "patch")
+    print(json.dumps(data))
+
+
+def bump_minor() -> str:
+    data = _bump_data(Repo(), "minor")
+    print(json.dumps(data))
+
+
+def bump_major() -> str:
+    data = _bump_data(Repo(), "major")
+    print(json.dumps(data))
 
 
 if __name__ == "__main__":
     repo = Repo()
     if len(sys.argv) == 1:
-        main(repo)
+        bump_patch()
     else:
-        main(repo, sys.argv[1])
+        print(json.dumps(_bump_data(repo, sys.argv[1])))
