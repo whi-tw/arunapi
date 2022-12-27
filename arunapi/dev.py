7b400d140d2a226188ac017b0ebe15684b54f18a
Tom Whitwell <tom@whi.tw>
2022-12-27 19:24:08 +0000

    that was dumb

--- arunapi/dev.py
+++ arunapi/dev.py
@@ -2,7 +2,12 @@ import os
 
 import uvicorn
 
+from . import __version__
+
 
 def run_dev():
     os.environ["ENVIRONMENT"] = "dev"
     uvicorn.run("arunapi.main:app", host="0.0.0.0", port=8000, reload=True)
+
+def current_version():
+    print(__version__)
